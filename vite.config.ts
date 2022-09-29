73734ae9f4137fb2c5723da821579bd9c6c04c57
alphatra <alpphatra@gmail.com>
2022-09-29 21:25:13 +0200

    readme.md + vite.config.ts update

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/Memory_Svelte/',
+  base:'/Memory_Game_Svelte/',
   plugins: [svelte()]
 })
